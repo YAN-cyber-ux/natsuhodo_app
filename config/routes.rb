39e2c85e151b188c1e5c0dbeb5ff06eaab183f24
yanger <kanayaofficial1129@gmail.com>
2024-03-14 21:34:44 +0900

    generate static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'staticpages/top'
+  get "static_pages/help"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
