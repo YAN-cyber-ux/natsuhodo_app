d96e4b47ac971bb247649b8446d601e1d5935d06
yanger <kanayaofficial1129@gmail.com>
2024-03-24 20:04:06 +0900

    debug

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,9 +29,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
-    assert_not is_logged_in?
     assert_response :see_other
+    assert_not is_logged_in?
     assert_redirected_to root_path
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
