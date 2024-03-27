4438de90728ebf87e40d1d75ac81038a6884c339
yanger <kanayaofficial1129@gmail.com>
2024-03-27 21:45:21 +0900

    Add destroy_user_test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,7 +42,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to user_url, status: :see_other
+    redirect_to users_url, status: :see_other
   end
 
   private
