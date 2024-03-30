e64f50aca611c1d424767b264a8b8e40e81ce05f
yanger <kanayaofficial1129@gmail.com>
2024-03-30 12:54:20 +0900

    Add privacy_policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get "/about", to: "static_pages#about"
+  get "/privacy_policy", to: "static_pages#privacy_policy"
   get "/contact", to: "static_pages#contact"
   get "/home", to: "static_pages#home"
   get "/help", to: "static_pages#help"
